499b3f7f687fc8b92ac1afa136d8ffe507466eed
yosuke112233 <vvggh102544@gmail.com>
2022-07-16 15:17:08 +0900

    make it better

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -78,6 +78,11 @@ import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 
 import com.example.common.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Slf4j
 @LineMessageHandler
